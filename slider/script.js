020b775b97cd6b3595587efdbe74a2a27c4c75a7
İsmail Baran <ismailbaran04@gmail.com>
2024-04-17 11:11:20 +0300

    bir models nesnesi oluşturdum ve models içeriğini dolddurdum ardından seçili olan indexe göre modelsdeki verileri aktardım

---
+++ slider/script.js
@@ -0,0 +1,21 @@
+var models = [
+    {
+        name: 'Bmw 116d',
+        image: 'img/bmw116d.jpg',
+        link:'https://www.sahibinden.com/ilan/vasita-otomobil-bmw-sahibinden-2019-139-binde-temiz-bmw-1166345085/detay'
+    },
+    {
+        name: 'F1 MERCEDES-AMG',
+        image: 'img/f1amg.jpg',
+        link:'https://www.mercedesamgf1.com/'
+    },
+    {
+        name: 'Mercedes-Benz',
+        image: 'img/mercedesamg.jpeg',
+        link:'https://www.sahibinden.com/ilan/vasita-otomobil-mercedes-benz-bayi-cikisli-orjinal-2023-amg-gt-63-se-performance-fl-20-kdv-1154955263/detay'
+    },
+]
+var index = 1;
+document.querySelector('.card-title').textContent = models[index].name;
+document.querySelector('.card-img-top').setAttribute('src', models[index].image)
+document.querySelector('.card-link').setAttribute('href', models[index].link);

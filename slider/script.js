aeaa0caf9596ba9797f26899e0445e94c6ad4181
İsmail Baran <ismailbaran04@gmail.com>
2024-04-17 12:05:56 +0300

    gerekli olan queryler oluşturuldu ve mouse  butonun üzerine gittiği taktirde slider akışı duruyor ve biz mouse u botunların üzerinden çekmeden slayt devam etmiyor

--- slider/script.js
+++ slider/script.js
@@ -13,12 +13,16 @@ var models = [
         name: 'Mercedes-Benz',
         image: 'img/mercedesamg.jpeg',
         link:'https://www.sahibinden.com/ilan/vasita-otomobil-mercedes-benz-bayi-cikisli-orjinal-2023-amg-gt-63-se-performance-fl-20-kdv-1154955263/detay'
-    },
+    }
 ]
 var index = 1;
 var slaytCount = models.length;
-
-showSlide(index);
+var interval;
+var settings = {
+    duration : '2000',
+    random : true
+};
+init(settings);
 
 document.querySelector('.fa-chevron-left').addEventListener('click', function(){
     index--;
@@ -30,14 +34,49 @@ document.querySelector('.fa-chevron-right').addEventListener('click', function()
     showSlide(index);
     console.log(index);
 });
+
+document.querySelectorAll('.arrow').forEach(function(item){
+    item.addEventListener('mouseenter', function(){
+       clearInterval(interval);  
+    })
+});
+
+document.querySelectorAll('.arrow').forEach(function(item){
+    item.addEventListener('mouseleave', function(){
+       init(settings); 
+    }) 
+});
+
+function init(settings){
+    var prev;
+   interval = setInterval(function(){
+        if(settings.random){
+            do{
+                index = Math.floor(Math.random() * slaytCount);
+            }while(index == prev)
+            prev = index;
+        }else{
+            if(slaytCount == index+1){
+                index = -1;
+            }
+            showSlide(index);
+            console.log(index);
+            index++;
+        }
+        showSlide(index);
+    },settings.duration)
+}
+
 function showSlide(i){
     index = i;
-    if(i < 0){
+    if (i < 0) {
         index = slaytCount - 1;
     }
-    if(i > slaytCount){
+    
+    if (i >= slaytCount) {
         index = 0;
     }
+    
 
 
     document.querySelector('.card-title').textContent = models[index].name;

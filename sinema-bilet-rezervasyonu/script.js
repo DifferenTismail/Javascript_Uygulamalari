b533504e642189bac25fc152d6e883f80f43171d
İsmail Baran <ismailbaran04@gmail.com>
2024-04-18 17:50:33 +0300

    son olarak bir local storage kayıt etme fonksiyonunu tamamladım ve local storagedan verileri ekrana getirme fonksiyonunu yazdım

--- sinema-bilet-rezervasyonu/script.js
+++ sinema-bilet-rezervasyonu/script.js
@@ -4,6 +4,8 @@ const amount = document.getElementById('amount');
 const select = document.getElementById('movie');
 const seats = document.querySelectorAll('.seat:not(.reserved)');
 
+getFromLocalStorage();
+calculateTotal();
 container.addEventListener('click', function(e){
     if(e.target.classList.contains('seat') && !e.target.classList.contains('reserved')){
         e.target.classList.toggle('selected');
@@ -39,6 +41,25 @@ function calculateTotal(){
     amount.innerText = selectedSeatCount * parseFloat(select.value);
     saveToLocalStorage(selectedSeatIndexs);
 }
+
+function getFromLocalStorage(){
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    if(selectedSeats != null && selectedSeats.length > 0){
+        seats.forEach(function(seat, index){
+            if(selectedSeats.indexOf(index) > -1){
+                seat.classList.add('selected')
+            }
+        });
+    }
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if(selectedMovieIndex != null){
+        select.selectedIndex = selectedMovieIndex;
+    }
+}
+
 function saveToLocalStorage(indexs){
-    localStorage.setItem('seşectedSeats', JSON.stringify(indexs))
+    localStorage.setItem('selectedSeats', JSON.stringify(indexs))
+    localStorage.setItem('selectedMovieIndex', select.selectedIndex)
 }
